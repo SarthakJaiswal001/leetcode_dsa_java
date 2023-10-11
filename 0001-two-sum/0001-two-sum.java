73e28317b5d887ef99450ca56876d6d1d6066734
SarthakJaiswal001 <96002671+SarthakJaiswal001@users.noreply.github.com>
2023-10-11 19:22:48 +0530

    Time: 50 ms (12.28%), Space: 43.2 MB (94.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,34 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       // Create a HashMap to store the elements and their indices
-        HashMap<Integer, Integer> numMap = new HashMap<>();
+//        // Create a HashMap to store the elements and their indices
+//         HashMap<Integer, Integer> numMap = new HashMap<>();
         
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
+//         for (int i = 0; i < nums.length; i++) {
+//             int complement = target - nums[i];
             
-            // If the complement is in the map, we found the solution
-            if (numMap.containsKey(complement)) {
-                return new int[]{numMap.get(complement), i};
+//             // If the complement is in the map, we found the solution
+//             if (numMap.containsKey(complement)) {
+//                 return new int[]{numMap.get(complement), i};
+//             }
+            
+//             // Otherwise, put the current number in the map with its index
+//             numMap.put(nums[i], i);
+//         }
+        
+//         // If no solution is found, return an empty array
+//         return new int[0];
+       
+        int n=nums.length;
+         int[] index=new int[2];
+        int k=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+              if(nums[i]+nums[j]==target){
+                  index[k]=i;
+                  index[k+1]=j;
+              }  
             }
-            
-            // Otherwise, put the current number in the map with its index
-            numMap.put(nums[i], i);
         }
-        
-        // If no solution is found, return an empty array
-        return new int[0];  
+        return index;
     }
 }
