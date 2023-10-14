871ea003e2504b64049d49bf500af2eac8f40729
SarthakJaiswal001 <96002671+SarthakJaiswal001@users.noreply.github.com>
2023-10-14 23:42:12 +0530

    Time: 10 ms (16.40%), Space: 73.4 MB (97.58%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+   // Define 2 vectors, one for storing positive 
+  // and other for negative elements of the array.
+        int n=nums.length;
+  ArrayList<Integer> pos=new ArrayList<>();
+  ArrayList<Integer> neg=new ArrayList<>();
+  
+  // Segregate the array into positives and negatives.
+  for(int i=0;i<n;i++){
+      
+      if(nums[i]>0) pos.add(nums[i]);
+      else neg.add(nums[i]);
+  }
+  
+  // Positives on even indices, negatives on odd.
+  for(int i=0;i<n/2;i++){
+      
+      nums[2*i] = pos.get(i);
+      nums[2*i+1] = neg.get(i);
+  }
+
+ 
+  return nums;
+    }
+}
