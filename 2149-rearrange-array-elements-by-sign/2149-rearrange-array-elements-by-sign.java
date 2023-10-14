481e186b2758c4ef86996245eaf5e4e9b10b4dde
SarthakJaiswal001 <96002671+SarthakJaiswal001@users.noreply.github.com>
2023-10-14 23:55:32 +0530

    Time: 4 ms (65.35%), Space: 84 MB (62.48%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,26 +1,40 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-   // Define 2 vectors, one for storing positive 
-  // and other for negative elements of the array.
-        int n=nums.length;
-  ArrayList<Integer> pos=new ArrayList<>();
-  ArrayList<Integer> neg=new ArrayList<>();
+//    // Define 2 vectors, one for storing positive 
+//   // and other for negative elements of the array.
+//         int n=nums.length;
+//   ArrayList<Integer> pos=new ArrayList<>();
+//   ArrayList<Integer> neg=new ArrayList<>();
   
-  // Segregate the array into positives and negatives.
-  for(int i=0;i<n;i++){
+//   // Segregate the array into positives and negatives.
+//   for(int i=0;i<n;i++){
       
-      if(nums[i]>0) pos.add(nums[i]);
-      else neg.add(nums[i]);
-  }
+//       if(nums[i]>0) pos.add(nums[i]);
+//       else neg.add(nums[i]);
+//   }
   
-  // Positives on even indices, negatives on odd.
-  for(int i=0;i<n/2;i++){
+//   // Positives on even indices, negatives on odd.
+//   for(int i=0;i<n/2;i++){
       
-      nums[2*i] = pos.get(i);
-      nums[2*i+1] = neg.get(i);
-  }
+//       nums[2*i] = pos.get(i);
+//       nums[2*i+1] = neg.get(i);
+//   }
 
  
-  return nums;
+//   return nums;
+        int n=nums.length;
+        int[] arr=new int[n];
+        int posnum=0,negnum=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                arr[negnum]=nums[i];
+                negnum+=2;
+            }
+            else{
+                arr[posnum]=nums[i];
+                posnum+=2;
+            }
+        }
+        return arr;
     }
 }
