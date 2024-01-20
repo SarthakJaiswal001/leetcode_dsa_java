d174636e5f40fff1689f89190685ec452a218176
SarthakJaiswal001 <96002671+SarthakJaiswal001@users.noreply.github.com>
2024-01-20 23:29:02 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (10.83%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = carry + x + y;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return dummyHead.next;
+    }
+     // Helper function to create a linked list from an array of integers
+    public ListNode createLinkedList(int[] nums) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+
+        for (int num : nums) {
+            current.next = new ListNode(num);
+            current = current.next;
+        }
+
+        return dummyHead.next;
+    }
+      // Helper function to print a linked list
+    public void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
